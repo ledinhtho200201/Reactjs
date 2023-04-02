253095d4cf37f6562d3bb1673dd61cdc564101f1
JRabbit <ledinhtho686@gmail.com>
2023-04-02 09:45:34 +0700

    90. manage specialty

--- src/services/userService.js
+++ src/services/userService.js
@@ -71,11 +71,15 @@ const postVerifyBookAppointment = (data) => {
     return axios.post('/api/verify-book-appointment', data)
 }
 
+const createNewSpecialty = (data) => {
+    return axios.post('/api/create-new-specialty', data)
+}
+
 export {
     handleLoginApi, getAllUsers, createNewUserService,
     deleteUserService, editUserService, getAllCodeService,
     getTopDoctorHomeService, getAllDoctors, saveDetailDoctorApi,
     getDetailInforDoctor, saveBulkScheduleDoctor, getScheduleDoctorByDate,
     getExtraInforDoctorById, getProfileDoctorById, postPatientBookAppointment,
-    postVerifyBookAppointment
+    postVerifyBookAppointment, createNewSpecialty
 }
